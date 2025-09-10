0608da99bc8e31950fd7b9696b3065dd9f4839dc
Mayur <mp9099644464@gmail.com>
2025-09-10 14:23:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern_crud").then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+const userSchema= new mongoose.Schema({
+    username:{type:String, required:true},
+    email:String,
+    number:{type:String, required:true,min:10,max:10},
+    message:{type:String, required:true},
+});
+
+
+
+const User= mongoose.model("User", userSchema);
+
+app.post("/users", async(req,res)=>{
+   try{
+     const user = await User.create(req.body);
+     res.json(user);
+   }
+    catch(err){
+        res.status(500).json({error: err.message}); 
+    }
+
+})
+
+app.get("/users", async(req,res)=>{
+    try{
+      const users= await User.find();
+        res.json(users);
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }})
+
+app.delete("/users/:id", async(req,res)=>{
+    try{
+        const user= await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).json({error: "User not found"});
+        }
+        res.json({message: "User deleted successfully"});
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+app.put("/users/:id", async(req,res)=>{
+    try{
+        const user= await User.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        if(!user){
+            return res.status(404).json({error: "User not found"});
+        }   
+        res.json(user);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+});
